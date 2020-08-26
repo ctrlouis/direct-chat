c163fad0f37228854db12c89e5a3a45e16032e9f
BourGuy64 <victor.bour.bowden@gmail.com>
2020-08-27 00:46:47 +0200

    Adjust messages bubble style

--- client/src/components/MessageList.vue
+++ client/src/components/MessageList.vue
@@ -1,8 +1,11 @@
 <template>
     <div>
         <div v-for="message in messages" :key="message.content" class="message" :class="messageSender(message.author)">
-            <md-avatar class="md-avatar-icon">{{avatarChar(message.author)}}</md-avatar>
-            {{message.author}} {{message.content}}
+            <span class="bubble">{{message.content}}</span>
+            <div class="meta-data">
+                <md-avatar class="md-avatar-icon">{{avatarChar(message.author)}}</md-avatar>
+                <span>{{message.author}}</span>
+            </div>
             <!-- <Message :author="message.author" :content="message.content" :date="message.data" /> -->
         </div>
     </div>
@@ -33,17 +36,40 @@ export default {
 .message {
     display: flex;
     flex-direction: row;
-    align-items: center;
+    align-items: baseline;
+    margin: 1em;
+
+    .bubble {
+        border: 1px solid black;
+        padding: .5em 2em;
+        border-radius: .3em;
+    }
+
+    .meta-data {
+        display: flex;
+        flex-direction: column;
+        margin: 0 1em;
+
+        span {
+            color: #a8a8a8;
+            font-style: italic;
+        }
+    }
 }
 
 .receive {
     align-self: start;
-    background-color: blue;
+    flex-direction: row-reverse;
+
+    .bubble {
+        color: white;
+        background: linear-gradient(rgb(0, 182, 255), rgb(0, 133, 255));
+    }
+    
 }
 
 .sent {
     align-self: end;
     background-color: white;
-    flex-direction: row-reverse;
 }
 </style>
